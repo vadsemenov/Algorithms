5831075ae0ac1522716ab9ebf77ebe6935f109e2
Vadim <5587394@mail.ru>
2022-02-02 21:30:34 +0300

    Adding Selection sort algorithms

---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    public class SelectionSort
+    {
+
+        public static void Main(String[] args)
+        {
+            int[] array = { 5, 6, 0, 1, 2, 4, 3 };
+            foreach (var digit in selectionSort(array))
+            {
+                Console.WriteLine(digit + " ");
+            }
+        }
+
+        public static int[] selectionSort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int jMin = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[jMin])
+                    {
+                        jMin = j;
+                    }
+                }
+                if (jMin != i)
+                {
+                    temp = array[i];
+                    array[i] = array[jMin];
+                    array[jMin] = temp;
+                }
+            }
+            return array;
+        }
+    }
+}
